05d47b80a3219cb5bf319ee3c0b301efb07bd7c6
hedgeinthehog <37834796+hedgeinthehog@users.noreply.github.com>
2021-04-27 19:53:50 +0300

    adds contacts' storing in localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,16 +5,23 @@ import ContactList from '../ContactList';
 
 class App extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      { id: 'id-5', name: 'Minion da Bob', number: '459-12-56' },
-      { id: 'id-6', name: 'Bobo', number: '443-89-12' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const localContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (localContacts) {
+      this.setState({ contacts: localContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (contacts.length !== prevState.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   updateFilter = event => {
     const { value } = event.target;
